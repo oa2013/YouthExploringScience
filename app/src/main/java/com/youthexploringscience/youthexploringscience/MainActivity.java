589a3716b70b2ce922afb3fc363402e5a6210199
JaeW <janice.wildrick@gmail.com>
2017-12-11 20:39:38 -0600

    feature:  implement NavigationDrawer feature and functionality

--- app/src/main/java/com/youthexploringscience/youthexploringscience/MainActivity.java
+++ app/src/main/java/com/youthexploringscience/youthexploringscience/MainActivity.java
@@ -16,19 +16,105 @@
 
 package com.youthexploringscience.youthexploringscience;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
- * Activity serving as app entry point
- *
+ * Activity serving as app entry point; buttons afford user options of opening the following
+ * web pages in a browser: paylocity, slsc.org, youthexploringscience.com
+ * & yes.com student resources
  */
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setUpNavDrawer();
+        setUpNavigationView();
+
+    }
+
+    /**
+     * initializes NavigationView & sets an ItemSelectedListener; handles click events
+     * for each item in NavigationView menu
+     */
+    private void setUpNavigationView() {
+        mNavigationView = findViewById(R.id.navigation_view);
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.nav_home:
+                        if (item.isChecked()) {
+                            return true;
+                        }
+                        //TODO if main activity is in back stack bring it to top of stack.
+                        // Otherwise, launch new main activity instance
+                        break;
+                    case R.id.nav_events:
+                        if (item.isChecked()) {
+                            return true;
+                        }
+                        //TODO launch new events activity
+                        break;
+                    case R.id.nav_contacts:
+                        //TODO  launch new contacts activity
+                        break;
+                    case R.id.nav_settings:
+                        //TODO launch preferences settings
+                        break;
+                }
+                adjustCheckedNavItems(item);
+                mDrawerLayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    /**
+     * Removes checked = true status for each MenuItem then sets
+     * checked = true for item passed as parameter
+     *
+     * @param item menu item selected
+     */
+    private void adjustCheckedNavItems(MenuItem item) {
+        int size = mNavigationView.getMenu().size();
+        for (int i = 0; i < size; i++) {
+            mNavigationView.getMenu().getItem(i).setChecked(false);
+        }
+        item.setChecked(true);
+    }
+
+    /**
+     * initialize DrawerLayout and set ActionBarDrawerToggle as listener to
+     * sync DrawerLayout state with ActionBar
+     */
+    private void setUpNavDrawer() {
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this,
+                mDrawerLayout, R.string.nav_layout_open, R.string.nav_layout_close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
